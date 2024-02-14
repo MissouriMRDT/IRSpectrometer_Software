17cbe82c310a4db5786f0c50226f8820109af84f
OcelotEmpire <hobbz.pi@gmail.com>
2024-02-14 02:37:39 -0600

    optical breadboard test code

--- Testing/optical_breadboard_vca/move_mirror/move_mirror.ino
+++ Testing/optical_breadboard_vca/move_mirror/move_mirror.ino
@@ -9,20 +9,27 @@
 // Define H bridge params
 int16_t offset1 = -10;
 int16_t offset2 = 10;
-uint32_t PWM = 750000000;
+uint16_t PWM = 50000;
 
 RoveHBridge motor1(FWD_PWM, RVS_PWM);
 
 void setup() {
-  Serial.begin();
+  Serial.begin(9600);
   delay(1000);
   Serial.println("Testing effect of vibrations from the VCA");
+  motor1.configFrequency(PWM);
+}
+
+int16_t notes[10] = {1000, 900, 800, 700, 600, 500, 600, 700, 800, 900};
+int16_t squareWave(uint32_t t) {
+  //return (t / 1000) % 2 == 0 ? 1000 : 500;
+  uint8_t index = (t/250) % 10;
+  return notes[index];
 }
 
 void loop() {
   uint32_t now = millis();
-  int16_t decipercent = std::sin((double)now * 0.001);
-  if(digitalRead(BUTTON) == HIGH) {
-    Motor1.drive(decipercent);
-  }
+  int16_t decipercent = squareWave(now);
+  motor1.drive(decipercent);
+  //Serial.println(decipercent);
 }
