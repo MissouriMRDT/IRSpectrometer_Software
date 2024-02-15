3c3228f090e0e74556cf0d198af08f3e9344186d
OcelotEmpire <hobbz.pi@gmail.com>
2024-02-14 23:11:20 -0600

    hacky code to make the motor move slower -- this requires changing configFrequency from taking a uint16_t to taking a float in RoveHBridge.h/cpp

--- Testing/optical_breadboard_vca/move_mirror/move_mirror.ino
+++ Testing/optical_breadboard_vca/move_mirror/move_mirror.ino
@@ -9,7 +9,7 @@
 // Define H bridge params
 int16_t offset1 = -10;
 int16_t offset2 = 10;
-uint16_t PWM = 50000;
+float PWM = 750000;
 
 RoveHBridge motor1(FWD_PWM, RVS_PWM);
 
@@ -20,16 +20,27 @@ void setup() {
   motor1.configFrequency(PWM);
 }
 
-int16_t notes[10] = {1000, 900, 800, 700, 600, 500, 600, 700, 800, 900};
+int interval = 10000;
+int minstren = 50, maxstren = 300;
+int diffstren = maxstren - minstren;
+int16_t notes[10] = {1000, 800, 600, 400, 200, 0, 200, 400, 600, 800};
 int16_t squareWave(uint32_t t) {
   //return (t / 1000) % 2 == 0 ? 1000 : 500;
-  uint8_t index = (t/250) % 10;
+  uint8_t index = (t/interval) % 10;
   return notes[index];
+}
+
+int16_t triangleWave(uint32_t t) {
+  uint32_t second = t / interval;
+  double frac = t - second * interval;
+  frac = frac / interval;
+  int16_t ret = second % 2 == 0 ? (int16_t)(maxstren - frac * diffstren) : (int16_t)(frac * diffstren + minstren);
+  return ret;
 }
 
 void loop() {
   uint32_t now = millis();
-  int16_t decipercent = squareWave(now);
+  int16_t decipercent = triangleWave(now);
   motor1.drive(decipercent);
   //Serial.println(decipercent);
 }
